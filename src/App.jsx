c5f3b41fac4168179652b4a055dfe5177494d67a
unknown <Everett.Horton95@yahoo.com>
2025-03-14 00:45:30 -0400

    most recent

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import {useState} from "react";
+import { puppyList } from "../data"; // Import the puppyList data
 
 function App() {
-  const [message, setMessage] = useState ("My first fluffy website");
+  const [puppies, setpuppies] = useState (puppyList);
+
+  // Console log the puppies daata to check if its loading 
+  console.log(puppies);
   return (
-    <div>
+    <div className="App">
       <h1>Furry Friends List</h1>
+      {puppies.map((puppy) => {
+        return <p key={puppy.id}>{}</p>
+      }
+
+      )}
       <p>My first React website!</p>
     </div>
   );
