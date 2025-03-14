d5ac413eb318447db84f34c5ae403ee9a870d971
unknown <Everett.Horton95@yahoo.com>
2025-03-14 00:14:29 -0400

    added featured puppy feature

--- src/FurryFriends.jsx
+++ src/FurryFriends.jsx
@@ -1,18 +1,29 @@
 import{useState} from "react";
 
-function FurryFriends() {
-    const [friends , setFriends] = useState([
+function App () {
+    const [furryFriends , setFriends] = useState([
         {id: 1, name: "Tracey", species: "Dog"},
         {id: 2, name: "Chelby", species: "cat"},
         {id: 3, name: "Blake" , species: "shark"}
     ]);
+
+    const [featuredFriends, setFeaturedFriend] = useState(null);
+
+    const selectFeatured = (friend) => {
+        setFeaturedFriend(friend);
+    }
 
     return (
         <div>
             <h2>Furry Friends</h2>
             <ul>
                 {friends.map((friend) => (
-                    <li key={friend.id}>
+                    <li key={friend.id}
+                    onDoubleClick={() => selectFeatured(friend)}
+                    style={{
+                        cursor: 'pointer',
+                        backgroundColor: setFeaturedFriend === friend ? '#f0f0f0' : 'transparent' 
+                    }}>
                         {friend.name} - {friend.species}
                     </li>
                 ))}
@@ -21,6 +32,5 @@ function FurryFriends() {
     );
 }
 
-export default FurryFriends
+export default App;
 
-console.log("FurryFriends")
